09c3fbc4d16e2870d93e7c8c133cf2d1599b8622
Lpslopes <lamartinelps@gmail.com>
2022-04-16 23:27:38 -0300

    Finalização_da_Interface

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 import Picker from './src/componets/Picker';
 
@@ -9,6 +9,33 @@ export default function App() {
       <View style={styles.areaMoeda}>
         <Text style={styles.titulo}>Selecione sua Moeda:</Text>
         <Picker/>  
+      </View>
+
+      <View style={styles.areaValor}>
+        <Text style={styles.titulo}>Digite um valor para converter em (R$)</Text>
+        <TextInput
+        placeholder="Ex: 150"
+        style={styles.input}
+        keyboardType='numeric'
+        />
+      </View>
+
+      <TouchableOpacity style={styles.botaoArea}>
+        <Text style={styles.botaoTexto}>Converter</Text>
+      </TouchableOpacity>
+
+      <View style={styles.areaResultado}>
+        <Text style={styles.valorConvertido}>
+          3 USD
+        </Text>
+        <Text style={[styles.valorConvertido, {fontSize: 18, margin: 10}]}>
+          Corresponde a
+        </Text>
+        <Text style={styles.valorConvertido}>
+          19,58
+        </Text>
+
+
       </View>
       
     </View>
@@ -22,4 +49,61 @@ const styles = StyleSheet.create({
     backgroundColor:'#101215',
     paddingTop: 40,
   },
+  areaMoeda: {
+    width:'90%',
+    backgroundColor:'#F9F9F9',
+    paddingTop: 9,
+    borderTopLeftRadius:9,
+    borderTopRightRadius:9,
+    marginBottom:1,
+  },
+  titulo: {
+    fontSize: 15,
+    color: '#000',
+    paddingTop: 5,
+    paddingLeft: 5,
+
+  },
+  areaValor:{
+    width:'90%',
+    backgroundColor: '#f9f9f9',
+    paddingBottom: 9,
+    paddingTop: 9,
+
+  },
+  input:{
+    width:'100%',
+    padding: 10,
+    height: 45, 
+    fontSize: 20,
+    marginTop: 9,
+    color: '#000',
+  },
+  botaoArea:{
+    width:'90%',
+    backgroundColor:'#fb4b57',
+    height: 45,
+    borderBottomLeftRadius: 9,
+    borderBottomRightRadius: 9,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botaoTexto:{
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  areaResultado:{
+    width:'90%',
+    backgroundColor:'#fff',
+    marginTop: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 25,
+  },
+  valorConvertido:{
+    fontSize: 39,
+    fontWeight: 'bold',
+    color: '#000',
+  }
 });
